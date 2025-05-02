93f4302b5c5de4ee91efc6f5f5cc9644bc33beb9
samy7777 <87314118+samy7777@users.noreply.github.com>
2025-05-03 01:23:28 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -95,7 +95,7 @@ const Login = () => {
                 <div className="flex flex-col gap-6">
                   <div className="flex flex-col items-center gap-5 text-center">
                     <h1 className="text-2xl font-bold">
-                      Login to your account Sam
+                      Login to your account Sa
                     </h1>
                     <p className="text-muted-foreground text-sm text-balance">
                       Enter your email below to login to your account 
